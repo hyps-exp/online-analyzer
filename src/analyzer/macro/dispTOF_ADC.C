8472970d2f2bcdb76e5c2211d42f1b04f72b2931
Rintaro Kurata <kurata.rintaro.s7@dc.tohoku.ac.jp>
2024-11-08 19:37:54 +0900

    Add macro, dispTOF_ADC

---
+++ src/analyzer/macro/dispTOF_ADC.C
@@ -0,0 +1,253 @@
+#include "DetectorID.hh"
+
+// Updater belongs to the namespace hddaq::gui
+using namespace hddaq::gui;
+
+#include "UserParamMan.hh"
+
+void dispTOF_ADC()
+{
+  const UserParamMan& gUser = UserParamMan::GetInstance();
+  // You must write these lines for the thread safe
+  // ----------------------------------
+  if(Updater::isUpdating()){return;}
+  Updater::setUpdating(true);
+  // ----------------------------------
+
+  // draw ADC U0-15
+  {
+    TCanvas *c = (TCanvas*)gROOT->FindObject("c1");
+    c->Clear();
+    c->Divide(4,4);
+    int adc_id     = HistMaker::getUniqueID( kTOF, 0, kADC,     0);
+    int adcwtdc_id = HistMaker::getUniqueID( kTOF, 0, kADCwTDC, 0);
+    for( int i=0; i<16; ++i ){
+      c->cd(i+1);
+      gPad->SetLogy();
+      TH1 *h = (TH1*)GHist::get( adc_id + i );
+      if( !h ) continue;
+      h->GetXaxis()->SetRangeUser( 0, 4096 );
+      h->Draw();
+      TH1 *hh = (TH1*)GHist::get( adcwtdc_id + i );
+      if( !hh ) continue;
+      hh->GetXaxis()->SetRangeUser( 0, 4096 );
+      hh->SetLineColor( kRed );
+      hh->Draw("same");
+    }
+    c->Update();
+  }
+
+  // draw ADC U16-31
+  {
+    TCanvas *c = (TCanvas*)gROOT->FindObject("c2");
+    c->Clear();
+    c->Divide(4,4);
+    int adc_id     = HistMaker::getUniqueID( kTOF, 0, kADC,     0);
+    int adcwtdc_id = HistMaker::getUniqueID( kTOF, 0, kADCwTDC, 0);
+    for( int i=16; i<31; ++i ){
+      c->cd(i+1);
+      gPad->SetLogy();
+      TH1 *h = (TH1*)GHist::get( adc_id + i );
+      if( !h ) continue;
+      h->GetXaxis()->SetRangeUser( 0, 4096 );
+      h->Draw();
+      TH1 *hh = (TH1*)GHist::get( adcwtdc_id + i );
+      if( !hh ) continue;
+      hh->GetXaxis()->SetRangeUser( 0, 4096 );
+      hh->SetLineColor( kRed );
+      hh->Draw("same");
+    }
+    c->Update();
+  }
+
+  // draw ADC U32-47
+  {
+    TCanvas *c = (TCanvas*)gROOT->FindObject("c3");
+    c->Clear();
+    c->Divide(4,4);
+    int adc_id     = HistMaker::getUniqueID( kTOF, 0, kADC,     0);
+    int adcwtdc_id = HistMaker::getUniqueID( kTOF, 0, kADCwTDC, 0);
+    for( int i=32; i<48; ++i ){
+      c->cd(i+1);
+      gPad->SetLogy();
+      TH1 *h = (TH1*)GHist::get( adc_id + i );
+      if( !h ) continue;
+      h->GetXaxis()->SetRangeUser( 0, 4096 );
+      h->Draw();
+      TH1 *hh = (TH1*)GHist::get( adcwtdc_id + i );
+      if( !hh ) continue;
+      hh->GetXaxis()->SetRangeUser( 0, 4096 );
+      hh->SetLineColor( kRed );
+      hh->Draw("same");
+    }
+    c->Update();
+  }
+
+  // draw ADC D0-15
+  {
+    TCanvas *c = (TCanvas*)gROOT->FindObject("c4");
+    c->Clear();
+    c->Divide(4,4);
+    int adc_id     = HistMaker::getUniqueID(kTOF, 0, kADC,     NumOfSegTOF);
+    int adcwtdc_id = HistMaker::getUniqueID(kTOF, 0, kADCwTDC, NumOfSegTOF);
+    for( int i=0; i<16; ++i ){
+      c->cd(i+1);
+      gPad->SetLogy();
+      TH1 *h = (TH1*)GHist::get( adc_id + i );
+      if( !h ) continue;
+      h->GetXaxis()->SetRangeUser( 0, 4096 );
+      h->Draw();
+      TH1 *hh = (TH1*)GHist::get( adcwtdc_id + i );
+      if( !hh ) continue;
+      hh->GetXaxis()->SetRangeUser( 0, 4096 );
+      hh->SetLineColor( kRed );
+      hh->Draw("same");
+    }
+    c->Update();
+  }
+
+  // draw ADC D16-31
+  {
+    TCanvas *c = (TCanvas*)gROOT->FindObject("c5");
+    c->Clear();
+    c->Divide(4,4);
+    int adc_id     = HistMaker::getUniqueID(kTOF, 0, kADC,     NumOfSegTOF);
+    int adcwtdc_id = HistMaker::getUniqueID(kTOF, 0, kADCwTDC, NumOfSegTOF);
+    for( int i=16; i<32; ++i ){
+      c->cd(i+1);
+      gPad->SetLogy();
+      TH1 *h = (TH1*)GHist::get( adc_id + i );
+      if( !h ) continue;
+      h->GetXaxis()->SetRangeUser( 0, 4096 );
+      h->Draw();
+      TH1 *hh = (TH1*)GHist::get( adcwtdc_id + i );
+      if( !hh ) continue;
+      hh->GetXaxis()->SetRangeUser( 0, 4096 );
+      hh->SetLineColor( kRed );
+      hh->Draw("same");
+    }
+    c->Update();
+  }
+
+  // draw ADC D32-47
+  {
+    TCanvas *c = (TCanvas*)gROOT->FindObject("c6");
+    c->Clear();
+    c->Divide(4,4);
+    int adc_id     = HistMaker::getUniqueID(kTOF, 0, kADC,     NumOfSegTOF);
+    int adcwtdc_id = HistMaker::getUniqueID(kTOF, 0, kADCwTDC, NumOfSegTOF);
+    for( int i=32; i<47; ++i ){
+      c->cd(i+1);
+      gPad->SetLogy();
+      TH1 *h = (TH1*)GHist::get( adc_id + i );
+      if( !h ) continue;
+      h->GetXaxis()->SetRangeUser( 0, 4096 );
+      h->Draw();
+      TH1 *hh = (TH1*)GHist::get( adcwtdc_id + i );
+      if( !hh ) continue;
+      hh->GetXaxis()->SetRangeUser( 0, 4096 );
+      hh->SetLineColor( kRed );
+      hh->Draw("same");
+    }
+    c->Update();
+  }
+
+  // //TOF TDC gate range
+  // static const unsigned int tdc_min = gUser.GetParameter("TdcTOF", 0);
+  // static const unsigned int tdc_max = gUser.GetParameter("TdcTOF", 1);
+
+  // // draw TDC U0-15
+  // {
+  //   TCanvas *c = (TCanvas*)gROOT->FindObject("c7");
+  //   c->Clear();
+  //   c->Divide(4,4);
+  //   int tdc_id = HistMaker::getUniqueID( kTOF, 0, kTDC );
+  //   for( int i=0; i<15; ++i ){
+  //     c->cd(i+1);
+  //     TH1 *h = (TH1*)GHist::get( tdc_id + i );
+  //     h->GetXaxis()->SetRangeUser( tdc_min, tdc_max );
+  //     if( h ) h->Draw();
+  //   }
+  //   c->Update();
+  // }
+
+  // // draw TDC U16-31
+  // {
+  //   TCanvas *c = (TCanvas*)gROOT->FindObject("c8");
+  //   c->Clear();
+  //   c->Divide(4,4);
+  //   int tdc_id = HistMaker::getUniqueID( kTOF, 0, kTDC );
+  //   for( int i=16; i<32; ++i ){
+  //     c->cd(i+1);
+  //     TH1 *h = (TH1*)GHist::get( tdc_id + i );
+  //     h->GetXaxis()->SetRangeUser( tdc_min, tdc_max );
+  //     if( h ) h->Draw();
+  //   }
+  //   c->Update();
+  // }
+
+  // // draw TDC U32-47
+  // {
+  //   TCanvas *c = (TCanvas*)gROOT->FindObject("c3");
+  //   c->Clear();
+  //   c->Divide(4,4);
+  //   int tdc_id = HistMaker::getUniqueID( kTOF, 0, kTDC );
+  //   for( int i=32; i<48; ++i ){
+  //     c->cd(i+1);
+  //     TH1 *h = (TH1*)GHist::get( tdc_id + i );
+  //     h->GetXaxis()->SetRangeUser( tdc_min, tdc_max );
+  //     if( h ) h->Draw();
+  //   }
+  //   c->Update();
+  // }
+
+  // // draw TDC D0-15
+  // {
+  //   TCanvas *c = (TCanvas*)gROOT->FindObject("c4");
+  //   c->Clear();
+  //   c->Divide(4,4);
+  //   int tdc_id = HistMaker::getUniqueID( kTOF, 0, kTDC, 1+NumOfSegTOF );
+  //   for( int i=0; i<16; ++i ){
+  //     c->cd(i+1);
+  //     TH1 *h = (TH1*)GHist::get( tdc_id + i );
+  //     h->GetXaxis()->SetRangeUser( tdc_min, tdc_max );
+  //     if( h ) h->Draw();
+  //   }
+  //   c->Update();
+  // }
+
+  // // draw TDC D16-31
+  // {
+  //   TCanvas *c = (TCanvas*)gROOT->FindObject("c4");
+  //   c->Clear();
+  //   c->Divide(4,4);
+  //   int tdc_id = HistMaker::getUniqueID( kTOF, 0, kTDC, 1+NumOfSegTOF );
+  //   for( int i=16; i<32; ++i ){
+  //     c->cd(i+1);
+  //     TH1 *h = (TH1*)GHist::get( tdc_id + i );
+  //     h->GetXaxis()->SetRangeUser( tdc_min, tdc_max );
+  //     if( h ) h->Draw();
+  //   }
+  //   c->Update();
+  // }
+
+  // // draw TDC D32-47
+  // {
+  //   TCanvas *c = (TCanvas*)gROOT->FindObject("c4");
+  //   c->Clear();
+  //   c->Divide(4,4);
+  //   int tdc_id = HistMaker::getUniqueID( kTOF, 0, kTDC, 1+NumOfSegTOF );
+  //   for( int i=32; i<48; ++i ){
+  //     c->cd(i+1);
+  //     TH1 *h = (TH1*)GHist::get( tdc_id + i );
+  //     h->GetXaxis()->SetRangeUser( tdc_min, tdc_max );
+  //     if( h ) h->Draw();
+  //   }
+  //   c->Update();
+  // }
+
+  // You must write these lines for the thread safe
+  // ----------------------------------
+  Updater::setUpdating(false);
+  // ----------------------------------
+}
